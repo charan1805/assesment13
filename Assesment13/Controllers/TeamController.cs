21ab73bcf07f70a961a340f44401a9efae9a2c74
charan1805 <sreecharanmudhiraj@gmail.com>
2023-08-29 23:07:34 +0530

    First class

---
+++ Assesment13/Controllers/TeamController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Assignment13.Controllers
+{
+    public class TeamController : Controller
+    {
+        // GET: Team
+
+        List<Team> listTeam = new List<Team>()
+        {
+            new Team(){Id=1,Name="India",NOWC= 2},
+            new Team(){Id=2,Name="Australia",NOWC=4},
+            new Team(){Id=3,Name="west Indies",NOWC=4},
+            new Team(){Id=4,Name="England",NOWC= 1},
+
+        };
+        public ActionResult Index()
+        {
+            return View(listTeam);
+        }
+
+    }
+}
---
+++ Assesment13/Models/Team.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Assignment13.Controllers
+{
+    public class Team
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int NOWC { get; set; }
+    }
+}
